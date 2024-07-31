f63d43636e1df02befea4099feff368a388b3207
Oluwabanke Eboda <abeegarl@gmail.com>
2024-08-01 00:45:30 +0100

    fix scrollToHome button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import star from '../assets/star.svg';
+import { IoChevronUp } from "react-icons/io5";
 
-export const Footer = ({scrollToHome}) => {
+export const Footer = ({ scrollToHome }) => {
     return (
         <footer className="flex flex-col md:flex-row items-center justify-between w-full px-[87px] py-4 md:py-6">
             <div className="flex items-center gap-3 mb-4 md:mb-0" onClick={scrollToHome}>
-            <img src={star} alt="Star icon" className="h-8"/>
-            <Link className="font-poppins text-2xl font-normal" to="/">AOE</Link>
+                <img src={star} alt="Star icon" className="h-8" />
+                <Link className="font-poppins text-2xl font-normal" to="/">AOE</Link>
             </div>
             <p className="font-poppins text-sm md:text-base text-center  justify-center md:text-left">
                 2024 © AOE. Crafted and Designed by Abigail Eboda.
             </p>
-            <div className="flex ">
-                <button onClick={scrollToHome} className="text-blue-500 hover:underline">
-                    Back to Top
-                </button>
-            </div>
+
+            <IoChevronUp className="h-6 w-6 cursor-pointer" onClick={scrollToHome} />
         </footer>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,9 @@ import React, { forwardRef } from 'react'
 const Projects = forwardRef((props, ref) => {
     return (
         <section id="projects" ref={ref} className='pt-16 md:pt-16'>
-            Projects</section>
+
+            <h1>Projects</h1>
+        </section>
     )
 })
 
